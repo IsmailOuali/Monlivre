a5c8fa249e3d697ec65dd3d386d0841e738cdc8e
unknown <ismail.ouali@ump.ac.ma>
2024-07-07 19:49:31 +0000

    adding the login and register front

--- MonLivre/resources/views/Auth/login.blade.php
+++ MonLivre/resources/views/Auth/login.blade.php
@@ -1,76 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
-        <link href="{{ asset('css/font-awesome.min.css')}}" rel="stylesheet">
-        <link href="{{ asset('css/style.css')}}" rel="stylesheet">        
 
-        <title>Document</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
 
-    <body>
-        <section class="form-07">
-            <div class="container">
-              <div class="row">
-                <div class="col-12">
-                  <div class="_form_07_main">
-                    <div class="row">
-                      <div class="col-sm-6 ncv-kl-bn">
-                        <div class="_form_07_main_sub_01">
-                          <div class="form-07-head">
-                            <h2>MonLivre</h2>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
-                          </div>
-                          <div class="form-7-social-media">
-                            <ol>
-                              <li><i class="fa fa-facebook"></i></li>
-                              <li><i class="fa fa-twitter"></i></li>
-                              
-                              <li><i class="fa fa-youtube"></i></li>
-                              <li><i class="fa fa-linkedin"></i></li>
-                            </ol>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-sm-6 cv-kl-bn">
-                        <div class="_pl_nb_df">
-                          <div class="_bg_cs">
-                            <h2>Sign into your account</h2>
-                          </div>
-                          <div class="form-group">
-                            <label>Enter Your Email</label>
-                            <input type="email" name="email" class="form-control" type="text" placeholder="Enter Email" required="" aria-required="true">
-                          </div>
-      
-                          <div class="form-group">
-                            <input type="password" name="password" class="form-control" type="text" placeholder="Enter password" required="" aria-required="true">
-                          </div>
-      
-                          <div class="checkbox form-group">
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="">
-                              <label class="form-check-label" for="">
-                                Remember me
-                              </label>
-                            </div>
-                            <a href="#">Register</a>
-                          </div>
-      
-                          <div class="form-group">
-                            <div class="_btn_04">
-                              <a href="#">Login</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+<body>
+
+    <div class="h-screen md:flex">
+        <div
+            class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+            <div>
+                <h1 class="text-white font-bold text-4xl font-sans">MonLivre</h1>
+                <p class="text-white mt-1">The most popular Library at Morocco</p>
+                <button type="submit"
+                    class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
             </div>
-          </section>
-    </body>
+            <div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8">
+            </div>
+            <div class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8">
+            </div>
+            <div class="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+            <div class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        </div>
+        <div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
+            <form class="bg-white">
+                <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
+                <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                    </svg>
+                    <input class="pl-2 outline-none border-none" type="text" name="" id=""
+                        placeholder="Email Address" />
+                </div>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <input class="pl-2 outline-none border-none" type="text" name="" id=""
+                        placeholder="Password" />
+                </div>
+                <button type="submit"
+                    class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
+                <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Register ?</span>
+            </form>
+        </div>
+    </div>
+</body>
+
 </html>
--- MonLivre/routes/web.php
+++ MonLivre/routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/login', function (){
     return view('/Auth/login');
 })->name('login');
+
+Route::get('/register', function (){
+    return view('/Auth/register');
+})->name('register');
